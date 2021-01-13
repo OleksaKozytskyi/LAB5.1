4b91fa66981d24d4ff63b725f08984dad00146bd
Oleksa Kozytskyy <askello_ua@MacBook-Air-Oleksa.local>
2021-01-13 16:53:31 +0200

    fgds

---
+++ cmake-build-debug/CMakeFiles/LABA5_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LABA5_1.dir/main.cpp.o"
+  "LABA5_1"
+  "LABA5_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LABA5_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
